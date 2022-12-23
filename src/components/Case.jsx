dd7f34bc4b6210ed7573016c8dd1a91db0feab8c
Clément Gateaud <clement.gateaud@gmail.com>
2022-12-23 22:59:25 +0100

    enum bug fix and click on blue

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -3,12 +3,13 @@ import styles from "./Case.module.css";
 
 export const Case = ({ owner, index, onClick }) => {
   return (
-    <div
-      className={classNamesMaker(styles.main, {
-        [styles["yellow"]]: owner === 1,
-        [styles["red"]]: owner === 2,
-      })}
-      onClick={onClick}
-    ></div>
+    <div className={styles.main} onClick={onClick}>
+      <div
+        className={classNamesMaker(styles.circle, {
+          [styles["yellow"]]: owner === 1,
+          [styles["red"]]: owner === 2,
+        })}
+      ></div>
+    </div>
   );
 };
