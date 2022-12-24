51c49f4f4031f2e12361cf501110985e571d2748
Clément Gateaud <clement.gateaud@gmail.com>
2022-12-24 22:46:26 +0100

    confetti

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import Confetti from "react-dom-confetti";
 import { whoIsTheWinner } from "../utils";
-import { useContext } from "react";
 import { Puissance4GameContext } from "../Puissance4GameContext";
 import { Case } from "./Case";
 import { ReactComponent as SupportSVG } from "../assets/support.svg";
 import styles from "./Grid.module.css";
 
 export const Grid = () => {
-  const { grid, setWinner, setIsDraw } = useContext(Puissance4GameContext);
+  const { grid, winner, setWinner, setIsDraw } = useContext(
+    Puissance4GameContext
+  );
 
   useEffect(() => {
     const { winner, isDraw } = whoIsTheWinner(grid);
@@ -15,8 +17,28 @@ export const Grid = () => {
     setIsDraw(isDraw);
   }, [grid, setWinner, setIsDraw]);
 
+  const CONFETTI_CONFIG = {
+    angle: 360,
+    spread: 360,
+    startVelocity: "20",
+    elementCount: 500,
+    dragFriction: 0.08,
+    duration: 4000,
+    stagger: 3,
+    width: "13px",
+    height: "13px",
+    perspective: "700px",
+    colors:
+      winner === 1
+        ? ["#b6c6109b", "#e8d607", "#a1a10d"]
+        : ["#ff2f2f", "#a21414", "#e85050"],
+  };
+
   return (
     <div className={styles.main}>
+      <div className={styles.confettiContainer}>
+        <Confetti active={winner} config={CONFETTI_CONFIG} />
+      </div>
       <div className={styles.gridBody}>
         <SupportSVG className={styles.support} fill="#197BBD" />
         <div className={styles.cases}>
